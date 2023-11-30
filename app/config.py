ddaab959dc7484c32b7d2241d13b95ddc0b23023
tabee <bee.mario@gmail.com>
2023-11-30 14:49:59 +0000

    typo

--- app/config.py
+++ app/config.py
@@ -3,11 +3,11 @@ import os
 import toml
 
 def _load_config():
-    ''' This function loads the config.toml file and returns it as a dictionary.'''
+    ''' This function loads the config.toml file and returns it as a dictionary. '''
     return toml.load("config.toml")
 
 def set_enviroment_variables():
-    ''' This function sets all the environment variables in the config.toml file. '''
+    ''' This function sets all the environment variables. '''
     config = _load_config()
     os.environ["OPENAI_API_KEY"] = config["keys"]["OPENAI_API_KEY"]
     os.environ["REDIS_URL"] = config["urls"]["REDIS_URL"]

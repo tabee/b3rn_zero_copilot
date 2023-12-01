8f69e1ae4f2ba69918f91b2fc485aed477fe9642
tabee <bee.mario@gmail.com>
2023-12-01 19:46:20 +0000

    call chuck_norris

--- langchain-container/chatbot/chuck_norris.py
+++ langchain-container/chatbot/chuck_norris.py
@@ -3,21 +3,21 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.schema import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough
 
-def chuck_norris_joke_about(topic):
+def chuck_norris_joke_about(topic="Elefanten"):
     prompt = ChatPromptTemplate.from_messages(
-            [
-                (
-                    "system",
-                    "erfinde einen Chuck Norris Witz über {topic}:\n\n",
-                ),
-                ("human", "{topic}"),
+        [
+            (
+                "system",
+                "erfinde einen Chuck Norris Witz über {topic}:\n\n",
+            ),
+            ("human", "{topic}"),
             ]
         )
-        model = ChatOpenAI(temperature=0.5)
-        runnable = (
-            {"topic": RunnablePassthrough()} | prompt | model | StrOutputParser()
-        )
+    model = ChatOpenAI(temperature=0.5)
+    runnable = (
+        {"topic": RunnablePassthrough()} | prompt | model | StrOutputParser()
+    )
 
-        result = runnable.invoke("Elefanten")
-        print(result)
+    result = runnable.invoke("Elefanten")
+    print(result)
     return result
--- langchain-container/chatbot/main.py
+++ langchain-container/chatbot/main.py
@@ -3,10 +3,15 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough
-from chatbot.chuck_norris import chuck_norris_joke_about    
+from chuck_norris import chuck_norris_joke_about
+
 
 app = FastAPI()
+
 @app.get("/")
 def read_root():
-    result  = chuck_norris_joke_about(topic="Elefanten")
-    return {"Joke": result}
+    res = chuck_norris_joke_about(topic="Pferde")
+   
+    print(res)
+
+    return {"Jocke": res}

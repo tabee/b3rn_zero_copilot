67b34e9137242dc5b87ba12635489dcaf25c9de2
tabee <bee.mario@gmail.com>
2023-12-04 17:38:12 +0000

    generalize the agent.py

--- langchain-container/chatbot/agent.py
+++ langchain-container/chatbot/agent.py
@@ -3,21 +3,21 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.schema import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough
 
-def run_agent(topic="Here can be your prompt!"):
+def run_agent(prompt="Here can be your prompt!"):
     prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
-                "you are a helpful agent. {topic}:\n\n",
+                "you are a helpful agent. {prompt}:\n\n",
             ),
-            ("human", "{topic}"),
+            ("human", "{prompt}"),
             ]
         )
     model = ChatOpenAI(temperature=0.5)
     runnable = (
-        {"topic": RunnablePassthrough()} | prompt | model | StrOutputParser()
+        {"prompt": RunnablePassthrough()} | prompt | model | StrOutputParser()
     )
 
-    result = runnable.invoke(topic)
+    result = runnable.invoke(prompt)
     print(result)
     return result

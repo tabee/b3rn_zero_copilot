4190964193f50c39245f1f07cd6f09deb815f445
Mario Bee <bee.mario@gmail.com>
2023-12-29 17:01:30 +0100

    haystack agent

--- haystack/app/24_building_chat_app.py
+++ haystack/app/24_building_chat_app.py
@@ -1,16 +1,23 @@
 ''' https://haystack.deepset.ai/tutorials/24_building_chat_app'''
 import os
 from getpass import getpass
-from haystack.telemetry import tutorial_running
 from haystack.nodes import PromptNode
 from haystack.agents.memory import ConversationSummaryMemory
 from haystack.agents.conversational import ConversationalAgent
 
-tutorial_running(24)
-
+#######################
+# we need to set the HF_API_KEY environment variable to access the model !!!!!!!!!!!!!!!!
+HF_API_KEY="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"
+os.environ["HF_API_KEY"] = HF_API_KEY   # needed for the Hub model to be accessible
+#######################
 model_api_key = os.getenv("HF_API_KEY", None) or getpass("Enter HF API key:")
+
 prompt_node = PromptNode(
-    model_name_or_path="HuggingFaceH4/zephyr-7b-beta", api_key=model_api_key, max_length=256, stop_words=["Human"]
+    model_name_or_path="HuggingFaceH4/zephyr-7b-beta",
+    api_key=model_api_key,
+    debug=True,
+    max_length=256,
+    stop_words=["Human"],
 )
 summary_memory = ConversationSummaryMemory(prompt_node)
 conversational_agent = ConversationalAgent(

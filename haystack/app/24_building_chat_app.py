8f4488f3690455efeae03501d436405a852b682d
Mario Bee <bee.mario@gmail.com>
2023-12-29 17:07:19 +0100

    fuiooo

--- haystack/app/24_building_chat_app.py
+++ haystack/app/24_building_chat_app.py
@@ -5,11 +5,6 @@ from haystack.nodes import PromptNode
 from haystack.agents.memory import ConversationSummaryMemory
 from haystack.agents.conversational import ConversationalAgent
 
-#######################
-# we need to set the HF_API_KEY environment variable to access the model !!!!!!!!!!!!!!!!
-HF_API_KEY="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"
-os.environ["HF_API_KEY"] = HF_API_KEY   # needed for the Hub model to be accessible
-#######################
 model_api_key = os.getenv("HF_API_KEY", None) or getpass("Enter HF API key:")
 
 prompt_node = PromptNode(
